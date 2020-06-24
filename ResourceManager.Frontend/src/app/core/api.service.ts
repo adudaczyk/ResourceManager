e8649a73d1e668f837d587bf48a938d3433e623b
Adrian Dudaczyk <a.dudaczyk@gmail.com>
2020-06-24 18:48:53 +0200

    Added reactive form to register component

--- ResourceManager.Frontend/src/app/core/api.service.ts
+++ ResourceManager.Frontend/src/app/core/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const localUrl = 'https://localhost:44320/';
 
@@ -16,5 +16,11 @@ export class ApiService {
 
   getResources() {
     return this.http.get(localUrl + 'api/Resources/GetResources');
+  }
+
+  postUser(data: []) {
+    var headers = new HttpHeaders({'content-type' : 'application/json'});
+    var body = JSON.stringify(data);
+    return this.http.post(localUrl + 'api/User', body, {headers: headers})
   }
 }
--- ResourceManager.Frontend/src/app/core/register/register.component.ts
+++ ResourceManager.Frontend/src/app/core/register/register.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
-    templateUrl: './register.component.html'
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+    registerForm: FormGroup;
+
+    constructor(private apiServive: ApiService) {
+    }
+
+    ngOnInit() {
+        this.registerForm = new FormGroup({
+            'firstName' : new FormControl(null, Validators.required),
+            'lastName' : new FormControl(null, Validators.required),
+            'phone' : new FormControl(null, Validators.required),
+            'email' : new FormControl(null, [Validators.required, Validators.email]),
+            'password' : new FormControl(null, [Validators.required, Validators.minLength(6)]),
+        });
+    }
+
+    onSubmit() {
+        this.apiServive.postUser(this.registerForm.value).subscribe(
+            (response) => console.log(response),
+            (error) => console.log(error)
+            )
+    }
 
 }
