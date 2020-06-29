376763392b2721ab4bdf80f4cb560219ee1860a3
Adrian Dudaczyk <a.dudaczyk@gmail.com>
2020-06-29 14:54:37 +0200

    Redirect from register and reset password page if already logged in

--- ResourceManager.Frontend/src/app/core/authorization/reset-password/reset-password.component.ts
+++ ResourceManager.Frontend/src/app/core/authorization/reset-password/reset-password.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../_services/api.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router  } from '@angular/router';
+import { AuthenticationService } from '../../_services/authentication.service';
 
 @Component({
     selector: 'app-reset-password',
@@ -10,7 +12,15 @@ export class ResetPasswordComponent implements OnInit {
 
     resetPasswordForm: FormGroup;
 
-    constructor(private apiServive: ApiService) {
+    constructor(
+        private apiServive: ApiService,
+        private router: Router,
+        private authenticationService: AuthenticationService) {
+        
+        // redirect to home if already logged in
+        if (this.authenticationService.currentUserValue) {
+            this.router.navigate(['/']);
+        }
     }
 
     ngOnInit() {
